166bc4a6804e6a1351500d5c6d38669a9c6ea739
najimiman <114479872+najimiman@users.noreply.github.com>
2022-12-15 15:05:51 +0100

    login

--- Réalisation-Projet4/api-projet4/routes/api.php
+++ Réalisation-Projet4/api-projet4/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\annee_formationController;
+use App\Http\Controllers\Formateurcontroller;
+use App\Models\Annee_formation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('Anneformation',annee_formationController::class);
+Route::get('/loginformateur/{email}/{cin}',[Formateurcontroller::class,'myshow']);
--- Réalisation-Projet4/react-p4/src/App.js
+++ Réalisation-Projet4/react-p4/src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './home/Home';
+import Login from './login/Login';
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  render(){
+    return(
+      <BrowserRouter>
+      <div>
+      <Link to='/'>Home</Link> {" "}
+      <Link to='/Login'>Login</Link>
+      <Routes>
+      <Route path="/" exact element={<Home/>}/>
+      <Route path="/Login"  element={<Login/>}/>
+      </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
