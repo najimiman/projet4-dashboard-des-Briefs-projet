d292e4df694bfc282b6a941fba4b0771f862b719
najimiman <114479872+najimiman@users.noreply.github.com>
2023-01-02 12:21:57 +0100

    react

--- Réalisation-Projet4/react-p4/src/home/Home.jsx
+++ Réalisation-Projet4/react-p4/src/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 class Home extends React.Component {
     state = {
-        list: [],Annee_scolaire:'',id:'',list1:[]
+        list: [],Annee_scolaire:'',id:'',Nom_groupe:''
     }
    
     Afficher = () => {
@@ -21,10 +21,11 @@ class Home extends React.Component {
         let Annee_scolaire=e.target.value;
        await axios.get('http://127.0.0.1:8000/api/mymethode/5/'+Annee_scolaire).then((res)=>{console.log(res.data);
        this.setState({
-        list1:res.data,id:res.data.id});
+        Nom_groupe:res.data.Nom_groupe,id:res.data.id});
         console.log(res.data.id);
+        console.log(res.data.Nom_groupe);
         })
-    
+        
     }
     render() {
         return (
@@ -36,19 +37,7 @@ class Home extends React.Component {
                     <option value={value.Annee_scolaire} key={value.id}>{value.Annee_scolaire}</option>
                 ))}
                 </select>
-                <table>
-                    <tbody>
-                        {this.state.list1.map((value)=>(
-                            <tr key={value.id}>
-                                <td><label name='id'>{value.id}</label> </td>
-                                <td><label>{value.Nom_groupe}</label></td>
-                                <td><label>{value.id_formateur}</label></td>
-                                <td><label>{value.Nom_formateur}</label></td>
-                                <td><label>{value.Annee_scolaire}</label></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        <label htmlFor="" name="Nom_groupe"> {this.state.Nom_groupe}</label>
             </div>
         );
     }
